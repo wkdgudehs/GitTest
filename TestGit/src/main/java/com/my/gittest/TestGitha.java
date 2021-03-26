fb8fdc7c2f4cb994fa9946000569cee346c86a73
김은준 <un2vers@gmail.com>
2021-03-26 23:18:31 +0900

    11:18
    깃허브 이제서야 풀과 푸쉬를 알게되었습니다

--- TestGit/src/main/java/com/my/gittest/TestGitha.java
+++ TestGit/src/main/java/com/my/gittest/TestGitha.java
@@ -5,6 +5,8 @@ public class TestGitha {
 		System.out.println("dddddd");
 		System.out.println("dddddd");
 		System.out.println("금욜 11시 14분 컴퓨터가 조금 느려졋");
+		System.out.println("금욜 11시 18분 깃 어렵");
+		
 		
 	}
 	
