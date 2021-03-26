50e8a7eeb75993789d26ff21064d97388b3e4a18
김은준 <un2vers@gmail.com>
2021-03-26 23:03:40 +0900

    test2
    ggg

--- TestGit/src/main/java/com/my/gittest/TestGitha.java
+++ TestGit/src/main/java/com/my/gittest/TestGitha.java
@@ -3,5 +3,8 @@ package com.my.gittest;
 public class TestGitha {
 	public static void main(String args[]) {
 		System.out.println("dddddd");
+		System.out.println("dddddd");
 	}
+	
 }
+//젝할게요 ㅋㅋㅋㅋㅋ 저폴더딧어?그건 모르니까지금 이자바파일을 수정해도 푸쉬되는지?확인?아~
