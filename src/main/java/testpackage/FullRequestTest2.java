9a2af70f3a4010fe5f8163223294849880c05ac8
Don <75960525+wkdgudehs@users.noreply.github.com>
2021-03-26 18:46:50 +0900

    제거

--- src/main/java/testpackage/FullRequestTest2.java
+++
@@ -1,7 +0,0 @@
-package testpackage;
-
-public class FullRequestTest2 {
-	public static void main(String args[]) {
-		System.out.println("풀리퀘스트2");
-	}
-}
--- src/main/java/testpackage/fullrequestTest.java
+++
@@ -1,7 +0,0 @@
-package testpackage;
-
-public class fullrequestTest {
-	public static void main(String args[]) {
-		System.out.println("테스트");
-	}
-}
